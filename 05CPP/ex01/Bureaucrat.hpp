4530a90aadf7f2f2a50bd0ab37ac4512fc6ab227
Manon LECUYER <mlecuyer@e2r4p3.clusters.42paris.fr>
2022-01-07 17:51:09 +0100

    begin ex01

---
+++ 05CPP/ex01/Bureaucrat.hpp
@@ -0,0 +1,45 @@
+#ifndef _BUREAUCRAT_HPP_
+# define _BUREAUCRAT_HPP_
+
+# include <stdexcept>
+# include <iostream>
+# include <string>
+
+class Bureaucrat
+{
+
+	class	GradeTooLowException : public std::exception 
+	{
+		public :
+			virtual const char* what() const throw();
+	};
+
+	class	GradeTooHighException : public std::exception 
+	{
+		public :
+			virtual const char* what() const throw();
+	};
+
+	public :
+
+		Bureaucrat(std::string name, unsigned int grade);
+		~Bureaucrat();
+		Bureaucrat(Bureaucrat const& cpy);
+		Bureaucrat&				operator=(const Bureaucrat& a);
+
+		std::string const &		getName(void) const;
+		unsigned int			getGrade(void) const;
+		void					upGrade(void);
+		void					downGrade(void);
+
+	private:
+
+		const std::string		_name;
+		unsigned int			_grade;
+
+
+};
+
+std::ostream & operator<<(std::ostream & out, Bureaucrat const & src);
+
+#endif
